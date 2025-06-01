c9e0cc7d306f767c192323ee5e43951b346095ef
Moufid <abilagbomoufid@gmail.com>
2025-06-01 17:10:59 +0200

    Changement

--- FAD/settings.py
+++ FAD/settings.py
@@ -16,6 +16,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -158,7 +160,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static')) ]
-MEDIA_URL = 'images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'backendimages','static')
 
 MEDIA_URL = 'images/'
