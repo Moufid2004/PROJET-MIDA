76242cabb0a7c5bd6bd5e7447c214302ef19ff20
Moufid <abilagbomoufid@gmail.com>
2025-06-01 17:35:24 +0200

    Changement

--- FAD/settings.py
+++ FAD/settings.py
@@ -159,8 +159,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static')) ]
+STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'staticfiles')) ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'backendimages','static')
+
+
 
 MEDIA_URL = 'images/'
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
