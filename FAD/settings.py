1ae8f0fa17e94a692917efc74361caabfbabffeb
Moufid <abilagbomoufid@gmail.com>
2025-06-01 18:56:04 +0200

    Changement

--- FAD/settings.py
+++ FAD/settings.py
@@ -38,6 +38,7 @@ ALLOWED_HOSTS = [
     "192.168.100.214",
     "192.168.1.76",
     "192.168.1.66",
+    'moufid.pythonanywhere.com'
     'projet-mida.onrender.com',
     '.vercel.app',
     '.now.sh',
